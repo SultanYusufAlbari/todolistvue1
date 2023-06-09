159434625f8a6e930b49c55098289f0ea01af60b
sultanyusufalbari@gmail.com <sultanyusufalbari@gmail.com>
2023-06-09 10:53:20 +0700

    Tugas Vue Sultan

--- app.vue
+++ app.vue
@@ -45,8 +45,8 @@
 </template>
 
 <script>
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 export default {
   data() {
       return{
